9f45ab4e3674f0071467b420b31219bb181baac2
DennisRono <dennisrkibet@gmail.com>
2022-07-05 00:57:36 +0300

    added poppins font

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react'
+import '../styles/css/header.css'
 import Helmet from 'react-helmet'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
@@ -10,8 +12,25 @@ const Header = () => {
         <link rel="canonical" href="http://tenafric.com" />
       </Helmet>
       <header className="header h-16">
-        <div className="container mx-auto">
-
+        <div className="xl:container mx-auto header-container">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="brand-logo">
+              <h1 className="logo-text">tenAfric</h1>
+            </div>
+            <div className="navigation">
+              <nav className="menu">
+                <ul className="flex">
+                  <li><Link to="#">Home</Link></li>
+                  <li><Link to="#">About</Link></li>
+                  <li><Link to="#">Services</Link></li>
+                  <li><Link to="#">Contact</Link></li>
+                </ul>
+              </nav>
+              <div className="header-cta">
+                <Link to="">Try Free</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
     </Fragment>
