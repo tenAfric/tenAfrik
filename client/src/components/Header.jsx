e6483fbccd654293582f1be39d70d41b4673e445
DennisRono <dennisrkibet@gmail.com>
2022-07-05 10:05:00 +0300

    header stylings

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,14 +12,14 @@ const Header = () => {
         <link rel="canonical" href="http://tenafric.com" />
       </Helmet>
       <header className="header h-16">
-        <div className="xl:container mx-auto header-container">
-          <div className="grid grid-cols-2 gap-4">
+        <div className="xl:container mx-auto header-container pt-4">
+          <div className="grid grid-cols-2 gap-4 header-wrapper items-center">
             <div className="brand-logo">
-              <h1 className="logo-text">tenAfric</h1>
+              <h1 className="logo-text font-bold text-4xl">tenAfric</h1>
             </div>
-            <div className="navigation">
+            <div className="navigation pt-2 flex gap-4 justify-end">
               <nav className="menu">
-                <ul className="flex">
+                <ul className="flex gap-4">
                   <li><Link to="#">Home</Link></li>
                   <li><Link to="#">About</Link></li>
                   <li><Link to="#">Services</Link></li>
