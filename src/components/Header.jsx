ea3811042df5ae955c3db5e4e5d4c8056eba1d12
DennisRono <dennisrkibet@gmail.com>
2022-07-06 12:18:01 +0300

    youth company say perhaps salmon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,6 +22,7 @@ const Header = () => {
           <div className="grid grid-cols-2 gap-4 header-wrapper items-center">
             <div className="brand-logo">
               <h1 className="logo-text font-bold text-4xl">tenAfric</h1>
+              <img src="../assets/images/white-logo.png" alt="" />
             </div>
             <div className="navigation pt-2 flex gap-6 justify-end">
               <nav className={(mobi)?"menu shwMobiMenu":"menu"}>
