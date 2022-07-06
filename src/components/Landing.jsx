f14f7f0c615fd345f4302a0c93af8412585c9e4b
DennisRono <dennisrkibet@gmail.com>
2022-07-06 12:13:01 +0300

    train attention evidence gave luck

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,10 +11,10 @@ const Landing = () => {
         <div className="landing-section mt-16">
           <div className="landing-wrapper flex">
             <div className="landing-texts mt-16">
-              <h1 className="landing-txt-header mt-8 text-6xl">Enlarge your market to greater horizons</h1>
+              <h1 className="landing-txt-header mt-8 text-6xl text-bold">Enlarge your market to greater horizons</h1>
               <p className="landing-txt-paragraph mt-8 text-[1.3rem]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea dolores ex, qui fugit amet cum.</p>
             </div>
-            <div className="landing-image">
+            <div className="landing-image mt-8">
               <LandingImg className="img-landing"/>
             </div>
           </div>
