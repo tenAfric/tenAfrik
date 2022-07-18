87ea14afff04941bb8a03396b7e97e4e84dcf1df
DennisRono <dennisrkibet@gmail.com>
2022-07-18 13:17:02 +0300

    pull flies powder blow cave

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -81,6 +81,9 @@ const Footer = () => {
             </ul>
           </div>
         </div>
+        <div className="footerCopyright">
+          
+        </div>
       </div>
     </Fragment>
   )
