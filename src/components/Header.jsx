c47e9defdc6883fc3f045d43e859fd8f53031fe4
DennisRono <dennisrkibet@gmail.com>
2022-07-18 13:56:01 +0300

    school husband came consonant oldest

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
                 </ul>
               </nav>
               <div className="header-cta">
-                <Link to="/contact" className="header-cta-btn px-4 py-2">Try Free</Link>
+                <Link to="/contact" className="header-cta-btn px-4 py-2">Get a Quote</Link>
               </div>
               <Bars className="openMobiMenu" onClick={()=>handleMobiMenu('open')}/>
             </div>
